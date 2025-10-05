63871fbe16a79f2f95c9c5ec430f99a358bcde36
tahrim sakib <you@example.comdmsakib1122@gmail.com>
2025-10-05 14:05:22 +0600

    2nd commit

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,28 +1,26 @@
+import { Star } from "lucide-react";
 import React from "react";
 
 const Book = ({ b }) => {
   console.log(b);
-  const {author,bookName,category ,image,publisher,rating,review} =b
+  const { author, bookName, category, image, publisher, rating, review } = b;
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <img
-          src={image}
-          alt="Shoes"
-        />
+    <div className="card bg-base-100 w-96 shadow-sm border p-4">
+      <figure className="p-3 bg-gray-200 w-4/5 mx-auto">
+        <img className="h-[166px]" src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          Card Title
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
+        <div>
+          <p>{category}</p>
+        </div>
+        <h2 className="text-2xl font-semibold">{bookName}</h2>
+        <h2 className="text-xl">{author}</h2>
+        <p>{review}</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+          <div className="badge badge-outline">{publisher}</div>
+          <div className="badge badge-outline">
+            {rating} <Star></Star>{" "}
+          </div>
         </div>
       </div>
     </div>
